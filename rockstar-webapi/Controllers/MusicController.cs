27b54d178d36cf8bd89a7d5c4e0773be318ebc66
Karn Vadaliya <karn.vadaliya@gmail.com>
2021-09-27 00:07:03 -0400

    All files

---
+++ rockstar-webapi/Controllers/MusicController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace rockstar_webapi.Controllers
+{
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        private Music myMusic = new Music();
+
+        private readonly ILogger<MusicController> _logger;
+
+        public MusicController(ILogger<MusicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]")]
+        public String Get()
+        {
+            return "<h1>My Jukebox!</h1>";
+        }
+
+        [HttpGet]
+        [Route("[controller]/Demons")]
+        public String GetDemons()
+        {
+            return myMusic.Demons;
+        }
+
+        [HttpGet]
+        [Route("[controller]/InYourEyes")]
+        public String GetInYourEyes()
+        {
+            return myMusic.InYourEyes;
+        }
+
+    }
+}
---
+++ rockstar-webapi/Music.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace rockstar_webapi
+{
+    public class Music
+    {
+        public string Demons { get; set; }
+
+        public string InYourEyes { get; set; }
+
+        public Music()
+        {
+            if(File.Exists("demons.txt"))
+            {
+                Demons = File.ReadAllText("demons.txt");
+            }
+            else
+            {
+                Demons = "??";
+            }
+            if (File.Exists("inyoureyes.txt"))
+            {
+                InYourEyes = File.ReadAllText("inyoureyes.txt");
+            }
+            else
+            {
+                InYourEyes = "??";
+            }
+        }
+    }
+}
